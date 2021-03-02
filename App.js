6819e38128c841338b2afd18e746e3e7dac2d0ef
Sourav Das <daazno1@Souravs-MacBook-Air.local>
2021-03-02 06:56:10 +0530

    error solve

--- App.js
+++ App.js
@@ -9,12 +9,10 @@
 import React from 'react';
 import {SafeAreaView,Text} from 'react-native';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 
 import authReducer from './src/store/reducers/auth';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -22,7 +20,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
 rootReducer, 
-composeEnhancers(applyMiddleware(thunk))
+applyMiddleware(thunk)
 );
 
 const App = () => {
